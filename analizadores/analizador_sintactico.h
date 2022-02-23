7c1490152b4f11ba3d73bd5084e47278a7277ea5
Andrea S. A <andrea.solla.alfonsin@rai.usc.es>
2022-02-23 19:32:48 +0100

    definiciones iniciado,  anal. sintactico hecho

---
+++ analizadores/analizador_sintactico.h
@@ -0,0 +1,10 @@
+#ifndef analizador_sintactico_h
+#define analizador_sintactico_h
+
+
+#include "../auxiliares/definiciones.h"
+
+void realizar_analisis();
+
+
+#endif
--- auxiliares/definiciones.h
+++ auxiliares/definiciones.h
@@ -3,9 +3,48 @@
 
 typedef struct comp_lexico
 {
-    int codigo;
-    char* cadena;
-}comp_lexico;
+    int componente_lexico;
+    char* lexema;
+} comp_lexico;
+
+
+#define KW_CHAN 300
+#define KW_FOR 301
+#define KW_FUNC 302
+#define KW_GO 303
+#define KW_IMPORT 304
+#define KW_PACKAGE 305
+#define KW_RANGE 306
+#define KW_VAR 307
+
+#define ID 400
+#define STRING 401
+#define INTEGER 402
+#define FLOAT 403
+#define COMPLEX 404
+
+#define TIPO_FLOAT32 450
+
+#define SEPARADOR_PAR_IZQ 500
+#define SEPARADOR_PAR_DER 501
+#define SEPARADOR_COR_IZQ 502
+#define SEPARADOR_COR_DER 503
+#define SEPARADOR_LLAVE_IZQ 504
+#define SEPARADOR_LLAVE_DER 505
+#define SEPARADOR_COMA 506
+#define SEPARADOR_PUNTO 507
+
+#define OPERADOR_ASIGNACION 600
+#define OPERADOR_INICIALIZACION 601
+#define OPERADOR_MAS_IGUAL 602
+#define OPERADOR_ENVIO 603
+#define OPERADOR_SUMA 604
+#define OPERADOR_RESTA 605
+#define OPERADOR_MULT 606
+#define OPERADOR_DIV 607
+#define OPERADOR_RANGO 608
+
+#define EOF 700
 
 
 #endif
